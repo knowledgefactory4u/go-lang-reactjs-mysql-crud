f83008b64e4fae1403606e130159ccc5018b6771
knowledgefactory4u <knowledgefactory4upeoples@gmail.com>
2022-01-23 11:41:30 +0530

    init

---
+++ backend/go.mod
@@ -0,0 +1,8 @@
+module backend
+
+go 1.17
+
+require (
+	github.com/go-sql-driver/mysql v1.6.0 // indirect
+	github.com/gorilla/mux v1.8.0 // indirect
+)
---
+++ backend/main.go
@@ -0,0 +1,194 @@
+package main
+
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	Routers()
+}
+
+func Routers() {
+	InitDB()
+	defer db.Close()
+	log.Println("Starting the HTTP server on port 9080")
+	router := mux.NewRouter()
+	router.HandleFunc("/users",
+		GetUsers).Methods("GET")
+	router.HandleFunc("/users",
+		CreateUser).Methods("POST")
+	router.HandleFunc("/users/{id}",
+		GetUser).Methods("GET")
+	router.HandleFunc("/users/{id}",
+		UpdateUser).Methods("PUT")
+	router.HandleFunc("/users/{id}",
+		DeleteUser).Methods("DELETE")
+	http.ListenAndServe(":9080",
+		&CORSRouterDecorator{router})
+}
+
+/***************************************************/
+
+//Get all users
+func GetUsers(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var users []User
+	result, err := db.Query("SELECT id, first_name," +
+		"last_name,email from users")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer result.Close()
+	for result.Next() {
+		var user User
+		err := result.Scan(&user.ID, &user.FirstName,
+			&user.LastName, &user.Email)
+		if err != nil {
+			panic(err.Error())
+		}
+		users = append(users, user)
+	}
+	json.NewEncoder(w).Encode(users)
+}
+
+//Create user
+func CreateUser(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	stmt, err := db.Prepare("INSERT INTO users(first_name," +
+		"last_name,email) VALUES(?,?,?)")
+	if err != nil {
+		panic(err.Error())
+	}
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		panic(err.Error())
+	}
+	keyVal := make(map[string]string)
+	json.Unmarshal(body, &keyVal)
+	first_name := keyVal["firstName"]
+	last_name := keyVal["lastName"]
+	email := keyVal["email"]
+	_, err = stmt.Exec(first_name, last_name, email)
+	if err != nil {
+		panic(err.Error())
+	}
+	fmt.Fprintf(w, "New user was created")
+}
+
+//Get user by ID
+func GetUser(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	result, err := db.Query("SELECT id, first_name,"+
+		"last_name,email from users WHERE id = ?", params["id"])
+	if err != nil {
+		panic(err.Error())
+	}
+	defer result.Close()
+	var user User
+	for result.Next() {
+		err := result.Scan(&user.ID, &user.FirstName,
+			&user.LastName, &user.Email)
+		if err != nil {
+			panic(err.Error())
+		}
+	}
+	json.NewEncoder(w).Encode(user)
+}
+
+//Update user
+func UpdateUser(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	stmt, err := db.Prepare("UPDATE users SET first_name = ?," +
+		"last_name= ?, email=? WHERE id = ?")
+	if err != nil {
+		panic(err.Error())
+	}
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		panic(err.Error())
+	}
+	keyVal := make(map[string]string)
+	json.Unmarshal(body, &keyVal)
+	first_name := keyVal["firstName"]
+	last_name := keyVal["lastName"]
+	email := keyVal["email"]
+	_, err = stmt.Exec(first_name, last_name, email,
+		params["id"])
+	if err != nil {
+		panic(err.Error())
+	}
+	fmt.Fprintf(w, "User with ID = %s was updated",
+		params["id"])
+}
+
+//Delete User
+func DeleteUser(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	stmt, err := db.Prepare("DELETE FROM users WHERE id = ?")
+	if err != nil {
+		panic(err.Error())
+	}
+	_, err = stmt.Exec(params["id"])
+	if err != nil {
+		panic(err.Error())
+	}
+	fmt.Fprintf(w, "User with ID = %s was deleted",
+		params["id"])
+}
+
+/***************************************************/
+
+type User struct {
+	ID        string `json:"id"`
+	FirstName string `json:"firstName"`
+	LastName  string `json:"lastName"`
+	Email     string `json:"email"`
+}
+
+//Db configuration
+var db *sql.DB
+var err error
+
+func InitDB() {
+	db, err = sql.Open("mysql",
+		"root:@tcp(127.0.0.1:3306)/userdb")
+	if err != nil {
+		panic(err.Error())
+	}
+}
+
+/***************************************************/
+
+// CORSRouterDecorator applies CORS headers to a mux.Router
+type CORSRouterDecorator struct {
+	R *mux.Router
+}
+
+func (c *CORSRouterDecorator) ServeHTTP(rw http.ResponseWriter,
+	req *http.Request) {
+	if origin := req.Header.Get("Origin"); origin != "" {
+		rw.Header().Set("Access-Control-Allow-Origin", origin)
+		rw.Header().Set("Access-Control-Allow-Methods",
+			"POST, GET, OPTIONS, PUT, DELETE")
+		rw.Header().Set("Access-Control-Allow-Headers",
+			"Accept, Accept-Language,"+
+				" Content-Type, YourOwnHeader")
+	}
+	// Stop here if its Preflighted OPTIONS request
+	if req.Method == "OPTIONS" {
+		return
+	}
+
+	c.R.ServeHTTP(rw, req)
+}
